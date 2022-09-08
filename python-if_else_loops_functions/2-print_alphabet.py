f62aa3f19a405ff3ff7144eccb03d88117e54873
EduRamos95 <eduramos08.30@gmail.com>
2022-09-08 18:00:52 -0500

    prints the ASCII alphabet, in lowercase (no new line)

---
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for a in range(ord('a'), ord('z') + 1):
+#for a in range(97, 122 + 1):
+#for a in range(97, 123):
+	print("{:c}".format(a), end='')
+#	print("{:s}".format(chr(a)), end="")
+#	print("{}".format(chr(a)), end='')
+#
+# print("".join(['{:c}'.format(c) for c in range(ord('a'), ord('z')+1)]), end="")
