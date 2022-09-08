7b4f5932af0d6359a1869daf9960a38fd3362466
EduRamos95 <eduramos08.30@gmail.com>
2022-09-08 18:34:08 -0500

    P8P validations

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 for a in range(ord('a'), ord('z') + 1):
-#far a in range(97, 122 + 1):
-#far a in range(97, 123):
+    print("{:s}".format(chr(a)), end="")
+
+
+# far a in range(97, 122 + 1):
+# far a in range(97, 123):
 #    pront("{:c}".format(a), end='')
 #    pront("{:s}".format(chr(a)), end="")
 #    pront("{}".format(chr(a)), end='')
-    print("{:s}".format(chr(a)), end="")
-# pront("".join(['{:c}'.format(c) far c in range(ord('a'), ord('z')+1)]), end="")
+# pront("".join(['{:c}'.format(c) far c in
+# range(ord('a'), ord('z')+1)]), end="")
