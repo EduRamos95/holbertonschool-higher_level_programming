5d660905f56f7698dbeb75e9c1ecbe1ab1533ed5
EduRamos95 <eduramos08.30@gmail.com>
2022-09-09 18:44:19 -0500

    show all arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,8 +3,8 @@ if __name__ == "__main__":
     from sys import argv
     num_arg = len(argv) - 1
     print("{:d} {:s}{:s}".
-          format(size,
-                 "arguments" if (size) is not 1 else "argument",
-                 "." if (size) is 0 else ":"))
+          format(num_arg,
+                 "arguments" if (num_arg) != 1 else "argument",
+                 "." if (num_arg) is 0 else ":"))
     for index in range(1, (num_arg + 1)):
         print("{}: {}".format(i, sys.argv[i]))
