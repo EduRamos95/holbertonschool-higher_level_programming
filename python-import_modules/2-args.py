e5f733a79929a70253fa68b055f7aef9b39b9e18
EduRamos95 <eduramos08.30@gmail.com>
2022-09-09 18:15:38 -0500

    prints the number of and the list of its arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ = "__main__":
+    from sys import argv
+#   user_input = argv[1:]
+    num_arg = len(argv) - 1
+    print("{:d} {:s}{:s}".
+          format(size,
+                 "arguments" if (size) is not 1 else "argument",
+                 "." if (size) is 0 else ":"))
+    for index in range(1, (num_arg + 1)):
+        print("{}: {}".format(i, sys.argv[i]))
