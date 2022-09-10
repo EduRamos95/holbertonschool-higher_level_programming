abec3a12c7dd294292a8972e337258b2ece5797e
EduRamos95 <eduramos08.30@gmail.com>
2022-09-09 19:24:08 -0500

    prints all the names defined by the compiled module

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     import hidden_4
     for word in dir(hidden_4):
         if word[:2] != "__":
-            print("{:s}".format(s))
+            print("{:s}".format(word))
