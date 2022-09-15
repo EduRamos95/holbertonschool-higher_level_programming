fb6665460106ae40ac968345d6ef050dad5a3445
EduRamos95 <eduramos08.30@gmail.com>
2022-09-14 23:46:50 -0500

    returns a key with the biggest integer value

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     result = 0
     winner = None
-    if a_dictionary is not None or len(a_dictionary) == 0:
+    if a_dictionary and len(a_dictionary):
         for key in a_dictionary:
             if a_dictionary[key] > result:
                 result = a_dictionary[key]
