d01bdf5b23c38f583ceecb69223cff9a6a96c6fa
EduRamos95 <eduramos08.30@gmail.com>
2022-09-26 18:26:19 -0500

    allows only attribute is called first_name

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""
+module - 101-locked_class
+Defines class with no class or object attribute
+Control dynamically created instance attributes
+"""
+
+
 class LockedClass:
+    """
+    prevents user from creating a new instance attribute 
+    dynamically unless attribute is "first_name"
+    """
+
     __slots__ = "first_name"
