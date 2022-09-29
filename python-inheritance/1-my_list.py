2a658055548653febcc7e6b3171aef4a02485e81
EduRamos95 <eduramos08.30@gmail.com>
2022-09-29 15:41:48 -0500

    add method that prints the list, but sorted (ascending sort)

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module 1-my_list
+Contains class MyList
+inherits fr_om list; has public instance method to print sorted
+"""
+
+
+class MyList(list):
+    """inherits fr_om list
+    methods:
+    print_sorted(self)
+    """
+    def print_sorted(self):
+        """prints list of ints all sorted in ascending order"""
+        print(sorted(self))
