62339aefb937026c6edbf7a3881797c79ee25f89
EduRamos95 <eduramos08.30@gmail.com>
2022-10-03 02:08:01 -0500

    inserts a line of text to a file, after each line containing a specific string

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -7,15 +7,16 @@ after each line containing a specific string
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """
-    Module Search and update
-    """
-    with open(filename, 'r+', encoding="utf-8") as fd:
-        lines = fd.readlines()
-        i = 0
-        for line in lines:
-            if line.find(search_string) != -1:
-                lines.insert(i + 1, new_string)
-            i += 1
-        fd.seek(0)
-        fd.write("".join(lines))
+    """appends "new_string" after a line containing
+    "search_string" in "filename" """
+    with open(filename, 'r+', encoding='utf-8') as f:
+        line_list = []
+        while True:
+            line = f.readline()
+            if line == "":
+                break
+            line_list.append(line)
+            if search_string in line:
+                line_list.append(new_string)
+        f.seek(0)
+        f.writelines(line_list)
