cfb17d100ee7f700159062aa4309c6b3bf5f9c5c
EduRamos95 <eduramos08.30@gmail.com>
2022-10-09 12:38:00 -0500

    fix PEP8 validation

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -7,17 +7,17 @@ T1: Contains private class __nb_objects, and class constructor __init__
 
 
 class Base():
-	"""
-	Class Attributes:
-	__nb_objects = 0
-	Class constructor:
-	def __init__(self, id=None):
-	"""
+    """
+    Class Attributes:
+    __nb_objects = 0
+    Class constructor:
+    def __init__(self, id=None):
+    """
 	__nb_objects = 0
 
-	def __init__(self, id=None):
-		if id is not None:
-			self.id = id
-		else:
-			Base.__nb_objects += 1
-			self.id = Base.__nb_objects
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
