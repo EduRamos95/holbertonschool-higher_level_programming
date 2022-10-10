6d6a1a460696a3d9d10c8e0b8812ce4d1a89fe3c
EduRamos95 <eduramos08.30@gmail.com>
2022-10-09 21:59:00 -0500

    adding static method from_json_string in base class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@ Module Base
 T1: Contains private class __nb_objects, and class constructor __init__
 T15: adding static method 'def to_json_string(list_dictionaries)'
 T16: adding class method 'def save_to_file(cls, list_objs)'
+T17: adding static method 'def from_json_string(json_string)'
 """
 import json
 
@@ -16,6 +17,9 @@ class Base():
         def __init__(self, id=None):
     Static method:
         def to_json_string(list_dictionaries):
+        def from_json_string(json_string):
+    Class method:
+        def save_to_file(cls, list_objs):
     """
     __nb_objects = 0
 
@@ -47,3 +51,11 @@ class Base():
                 list_dict.append(cls.to_dictionary(obj))
         with open(filename, "w", encoding="utf-8") as fd:
             fd.write(cls.to_json_string(list_dict))
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        return python obj of JSON string representation
+        """
+        if json_string is None or len(json_string) == 0:
+            json_string = "[]"
+        return (json.loads(json_string))
