8bf78b40887f3cc2daf1ef2e905bb6f31a6219fe
EduRamos95 <eduramos08.30@gmail.com>
2022-11-05 18:14:49 -0500

    Full description

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,8 @@
+-- print full description of table; without using 'DECSCRIBE [tableName]'
+-- Table   Create Table
+-- first_table CREATE TABLE `first_table` 
+-- `id` int(11)
+-- `name` varchar(256)
+-- ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+SHOW CREATE TABLE first_table;
