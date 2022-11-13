31112bb776fea74b0511fc6324e57d1db177405a
EduRamos95 <eduramos08.30@gmail.com>
2022-11-13 17:07:23 -0500

    creates the table id_not_null (ID can't be null)

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -2,7 +2,7 @@
 -- description of data in table: id INT with the default value 1, name VARCHAR(256)
 -- should not fail if exists
 
-CREATE TABLE IF NOT EXISTS id_not_null (
+CREATE TABLE IF NOT EXISTS 'id_not_null' (
 		'id' INT DEFAULT 1,
 		'name' VARCHAR(256)
-);
+		);
