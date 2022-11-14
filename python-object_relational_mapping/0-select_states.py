b44381f7ac3ca12afbfe27d3255758ef77acad38
EduRamos95 <eduramos08.30@gmail.com>
2022-11-14 00:03:02 -0500

    fix code

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,17 +4,16 @@ return all table values (table 'states')
 parameters given to script: username, password, database
 """
 
-import MySQLdb
-from sys import argv
 
 if __name__ == "__main__":
-
+	import MySQLdb
+	from sys import argv
     # connect to database
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
+                         password=argv[2],
+                         database=argv[3])
 
     # create cursor to exec queries using SQL
     cursor = db.cursor()
