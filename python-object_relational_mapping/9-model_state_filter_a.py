2d1d1c8c4a278885a1dc62efcf0d63333bfb11a2
EduRamos95 <eduramos08.30@gmail.com>
2022-11-16 16:51:34 -0500

    fix: document module

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -9,13 +9,14 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import (sessionmaker)
 
 if __name__ == "__main__":
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     state_table = Session().query(State)
-    .filter(State.name.like('%a'))
-    .order_by(State.id).all()
+        .filter(State.name.like('%a'))
+        .order_by(State.id).all()
     for state in state_table:
         print("{}: {}".format(state.id, state.name))
     Session().close()
