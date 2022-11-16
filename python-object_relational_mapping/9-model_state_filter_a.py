48b440ef285b6e5be3e95ec163daaefaf3e63a32
EduRamos95 <eduramos08.30@gmail.com>
2022-11-16 16:45:22 -0500

    lists all State objects that contain the letter a from the database hbtn_0e_6_usa

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -3,9 +3,10 @@
 lists all State objects that contain the letter a
 from the database hbtn_0e_6_usa
 """
+
+
 import sys
 from model_state import Base, State
-
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import (sessionmaker)
 
