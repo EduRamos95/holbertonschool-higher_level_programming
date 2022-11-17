ec7f2490009c5a0ef6df4e6778727943b52c256e
EduRamos95 <eduramos08.30@gmail.com>
2022-11-16 19:32:46 -0500

    fix: var state_table

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-lists all State objects that contain 
+lists all State objects that contain
 the letter a from the
 database hbtn_0e_6_usa
 """
@@ -18,10 +18,10 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     state_table = Session().query(State)\
-        .filter(State.name == sys.argv[4]).count()
-    if stable_table == 0:
+        .filter(State.name == sys.argv[4])
+    if (state_table.count() == 0):
         print("Not found")
     else:
-        for state in Session().query(State).filter_by(name=sys.argv[4]):
-            print("{}".format(state.id))
+        for element in state_table:
+            print("{}".format(element.id))
     Session().close()
