28dec90d6e54eb71f0804885c5bf526c8c0af6da
EduRamos95 <eduramos08.30@gmail.com>
2022-12-16 18:50:46 -0500

    fix: semistandard

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  const index_max = list.length - 1;
+  const indexMax = list.length - 1;
   const reverse = [];
-  for (let i = index_max; i >= 0; i--) {
+  for (let i = indexMax; i >= 0; i--) {
     reverse.push(list[i]);
   }
   return reverse;
