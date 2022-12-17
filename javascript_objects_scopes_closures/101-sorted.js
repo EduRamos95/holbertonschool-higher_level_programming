7daff5b21566a983ddb7cb8f41672f1f4056a71a
EduRamos95 <eduramos08.30@gmail.com>
2022-12-16 21:28:34 -0500

    script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence

---
+++ javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const dictInput = require('./101-data.js').dict;
+const dictOut = {};
+for (const key in dictInput) {
+  if (dictOut[`${dictInput[key]}`] === undefined) {
+    dictOut[`${dictInput[key]}`] = [`${key}`];
+  }
+  else {
+    dictOut[`${dictInput[key]}`].push(`${key}`);
+  }
+}
+console.log(dictOut);
