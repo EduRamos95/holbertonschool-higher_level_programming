6c5d8a2ed0066d5bf8a0838a7a3b72b8ede022a7
EduRamos95 <eduramos08.30@gmail.com>
2022-12-18 12:51:03 -0500

    fix: semistandard

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -6,23 +6,23 @@ request(url, (err, response, body) => {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
-    let new_key;
-    let new_value;
-    let d_exp = {};
-    let d_in = JSON.parse(body);
-    for (d_get of d_in) {
-      if (d_get["completed"]) {
-        new_key = "" + d_get["userId"];
-        if (!d_exp[new_key]) {
-          d_exp[new_key] = 1;
+    let newKey;
+    let newValue;
+    const dExp = {};
+    const dIn = JSON.parse(body);
+    for (const dGet of dIn) {
+      if (dGet.completed) {
+        newKey = '' + dGet.userId;
+        if (!dExp[newKey]) {
+          dExp[newKey] = 1;
         } else {
-          new_value = d_exp[new_key];
-          d_exp[new_key] = new_value + 1;
+          newValue = dExp[newKey];
+          dExp[newKey] = newValue + 1;
         }
       }
     }
-    console.log(d_exp);
+    console.log(dExp);
   } else {
     console.log(`Error: ${response.statusCode}`);
   }
-  });
+});
